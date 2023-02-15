c5e22b1bdb5286a80025806f292b9863680bbb08
Karl <karlbackworth@hotmail.com>
2023-02-15 22:26:06 +0000

    Added new file helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+# Importing modules
+import time
+
+# Print Statement
+statement = "Hello World!"
+
+# Print Statement
+for i in statement:
+    time.sleep(0.1)
+    print(i, end = '', flush = True)
+time.sleep(1)
+
